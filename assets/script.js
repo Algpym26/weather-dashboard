3d7636e6b68b96c943d126b95cf14d62230bc3d6
Algpym26 <110873247+Algpym26@users.noreply.github.com>
2022-10-29 20:26:43 -0400

    create card flexbox

--- assets/script.js
+++ assets/script.js
@@ -13,7 +13,7 @@ var currentTemp=document.querySelector(".temp")
 var currentHum=document.querySelector(".hum")
 var currentWind=document.querySelector(".wind")
 var currentUvindex=document.querySelector(".uvindex")
-var forescastContainer=document.querySelector(".forestcas")
+var forescastContainer=document.querySelector(".forecast")
 
 //element.addEventListener(event, listener, useCapture);
 searchButton.addEventListener("click", function(event){
@@ -44,8 +44,10 @@ fetch(locationURL)
     currentHum.textContent="Hum: "+data.main.humidity
     currentWind.textContent="Wind: "+data.wind.speed
     currentUvindex.textContent="Description: "+data.weather[0].description
+    fivedays(data.coord.lat, data.coord.lon)
 })
 }
+
 
 //<button type="button"
 //onclick="document.getElementById('demo').innerHTML = Date()">
@@ -54,3 +56,40 @@ fetch(locationURL)
 //<p id="demo"></p>//
 
 //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+function fivedays(lat, lon){
+    var forecastURL="https://api.openweathermap.org/data/2.5/forecast?lat="+ lat +"&lon="+ lon +"&appid=81ccc9c6a08c941e92df0ad282737a69&units=imperial"
+
+    fetch(forecastURL)
+    .then (function(respond){
+        return respond.json()
+
+    })
+    .then (function(data){
+        console.log (data)
+        for (var i=0; i<data.list.length; i=i+8){
+            console.log (data.list[i])
+            var card=document.createElement("div")
+            card.setAttribute("class","card")
+            var temp=document.createElement("h3")
+            temp.textContent= "temp:" +data.list[i].main.temp
+            card.appendChild(temp)
+            var hum=document.createElement("h3")
+            hum.textContent= "hum:" +data.list[i].main.humidity
+            card.appendChild(hum)
+            var wind=document.createElement("h3")
+            wind.textContent= "wind:" +data.list[i].wind.speed
+            card.appendChild(wind)
+            forescastContainer.appendChild(card)
+
+
+        }
+
+    })
+}
+let i = 0;
+let len = card.length;
+let text ="";
+for (; i < len; ) {
+    text += card[i] + "<br>";
+    i++;
+}
